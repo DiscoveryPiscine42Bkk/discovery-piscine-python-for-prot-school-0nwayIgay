cf26410dc3750094068ee1a24af33aab63daa6b0
0nwayIgay <40340@prot.ac.th>
2025-03-10 15:34:36 +0700

    Add files via upload

---
+++ cell02/ex21/isneg.py
@@ -0,0 +1,10 @@
+x = int(input("GO GO INPUT NUMBER : "))
+
+if x < 0:
+    print("this number is negative.")
+
+elif x > 0:
+    print("this number is positive.")
+
+elif x == 0 :
+    print("this number is both positive and negative.")
