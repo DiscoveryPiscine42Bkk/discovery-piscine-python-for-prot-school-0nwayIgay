3bd82d1edb9e5e2b83a8051155edc6b8afabae10
0nwayIgay <40340@prot.ac.th>
2025-03-11 14:18:52 +0700

    Add files via upload

---
+++ cell04/ex41/age.py
@@ -0,0 +1,6 @@
+a = int(input("Please tell me your age : "))
+print(f"you are currently {a} years old.")
+
+print(f"In 10 years, you'll be {a+10} years old.")
+print(f"In 20 years, you'll be {a+20} years old.")
+print(f"In 30 years, you'll be {a+30} years old.")
