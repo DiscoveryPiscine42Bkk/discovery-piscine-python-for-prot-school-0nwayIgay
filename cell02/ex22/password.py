a1feb019f4537848f5f6ac4b6a363c6a44676271
0nwayIgay <40340@prot.ac.th>
2025-03-11 09:23:25 +0700

    Add files via upload

---
+++ cell02/ex22/password.py
@@ -0,0 +1,8 @@
+inputpassword = str(input("input password"))
+password = ("Python is awesome")
+
+if inputpassword == "Python is awesome":
+    print("ACCESS GRANTED")
+
+else :
+    print("ACCESS DENIED")
