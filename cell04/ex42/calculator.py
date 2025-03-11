986ce86b7443138c5f557162c727ab5f8bb4a69c
0nwayIgay <40340@prot.ac.th>
2025-03-11 14:28:18 +0700

    Add files via upload

---
+++ cell04/ex42/calculator.py
@@ -0,0 +1,8 @@
+x = int(input("Give me the first number : "))
+y = int(input("Give me the second number : "))
+print("Thank you!")
+
+print(f"{x} + {y} = {x+y}")
+print(f"{x} - {y} = {x-y}")
+print(f"{x} / {y} = {x/y}")
+print(f"{x} * {y} = {x*y}")
