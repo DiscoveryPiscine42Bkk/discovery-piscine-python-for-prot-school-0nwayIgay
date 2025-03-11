f25968c99943a3d2a3ee78fec157dff55b84bfb9
0nwayIgay <40340@prot.ac.th>
2025-03-11 13:23:11 +0700

    Add files via upload

---
+++ cell03/ex32/i_got_that.py
@@ -0,0 +1,8 @@
+frstans = str(input("What you gotta say? :"))
+scdans = str(input("I got that! Anything else? :"))
+
+while scdans != "STOP":
+    scdans = str(input("I got that! Anything else? :"))
+    
+    if scdans == "STOP":
+            break # stop the loop
